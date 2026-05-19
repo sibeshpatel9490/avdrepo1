32b59054e595b727d5e539e8095cf681b03a3ac2
Sibesh Patel <sibeshpatel@gmail.com>
2026-05-19 20:06:30 +0530

    test-9

--- test_calc.py
+++ test_calc.py
@@ -7,8 +7,8 @@ def test_add():
 def test_sub():
     assert sub(10, 5) == 5
 
-def test_mul():
-    assert mul(10, 20) == 200
+# def test_mul():
+#     assert mul(10, 20) == 200
 
-def test_div():
-    assert div(10, 2) == 5
+# def test_div():
+#     assert div(10, 2) == 5
